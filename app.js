958ffef4b328ff848d7c4087bccdf93443d552b1
Sponicus <andrew.spon@gmail.com>
2022-06-07 15:15:50 -0400

    fix error by installing EJS

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render("home")
+  res.render("home");
 })
 
-app.listen(3000)
+
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log(`XP Calculator listening to ${PORT}`);
+});
